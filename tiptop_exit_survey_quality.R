f7eb6f42494a3726826c71c0fd3da507923ca4e5
maxramirez84 <maxramirez84@gmail.com>
2020-11-09 15:41:01 +0100

    Implemented new function for retrieving the data either from a data file exported from REDCap or directly through the REDCap API.

--- tiptop_exit_survey_quality.R
+++ tiptop_exit_survey_quality.R
@@ -1,8 +1,5 @@
 library(redcapAPI)
 
-# Auxiliar functions -----------------------------------------------------------
-
-# Data timestamp
 DataTimestamp <- function(data.retrieval.mode, file.date = "", file.time = "") {
   # Returns the file timestamp if we are retrieving the data from a file and the
   # current datetime if we are extrating the data through the REDCap API.
@@ -30,4 +27,51 @@ DataTimestamp <- function(data.retrieval.mode, file.date = "", file.time = "") {
   }
   
   return(data.timestamp)
+}
+
+
+ReadData <- function(data.retrieval.mode, file.prefix = "", 
+                     file.content = "_DATA_", file.date = "", file.time = "", 
+                     api.url = "", api.token = "", 
+                     non.retrieved.variables = "") {
+  # Retrieve data from a CSV file (downloaded from REDCap) or directly from 
+  # REDCap through the API.
+  #
+  # Args:
+  #   data.retrieval.mode: String {file, api} indicating how we're retrieving 
+  #                        the data.
+  #   file.prefix: String idicating the path to the data file. Usually 
+  #                DATA/DATA_WITH_NO_DUPS/TIPTOPExitSurvey<Country>
+  #   file.content: String indicating the middle part of the file name which
+  #                 indicates if the data contains duplicates or not. Usually
+  #                 either _DATA_ or _DATA_WITH_NO_DUPS_ or other 
+  #                 data transformation.
+  #   file.date: String indicating the date of the data in the file.
+  #   file.time: String indicating the time of the data in the file.
+  #   api.url: String indicating the URL of the REDCap API where the data is.
+  #   api.token: String indicating the token for accessing the REDCap API.
+  #   non.retireved.variables: Vector containing the list of fields or variables
+  #                            to be excluded during the data export.
+  # Returns:
+  #   Data frame with the dataset.
+  if (data.retrieval.mode == "file") {
+    data.filename <- paste0(file.prefix, file.content, file.date, "_", 
+                            gsub(":", "", file.time), ".csv")
+    data <- read.csv(data.filename, stringsAsFactors = FALSE)
+  } 
+  else if (data.retrieval.mode == "api") {
+    rcon <- redcapConnection(api.url, api.token)
+    field.names <- exportFieldNames(rcon)
+    fields <- unique(
+      field.names$original_field_name[! field.names$original_field_name %in% 
+                                        non.retrieved.variables])
+    data <- exportRecords(rcon, factors = F, fields = fields)
+  }
+  else {
+    message("data.retrieval.mode must be either file or api. No more modes 
+            available.")
+    return(NULL)
+  }
+  
+  return(data)
 }
