a4051fad0229b7d548e26e8cd1dd07818d15cd39
maxramirez84 <maxramirez84@gmail.com>
2020-11-11 11:23:29 +0100

    Extracted all language related strings to lang.R

---
+++ lang.R
@@ -0,0 +1,17 @@
+kLang <- list(
+  EN = list(
+    head.survey        = "TIPTOP ANC Exit Survey",
+    head.report        = "Data Quality Report"
+  ),
+  FR = list(
+    head.survey        = "TIPTOP Enquête de Sortie de la CPN",
+    head.report        = "Rapport de Qualité de Données",
+    head.github        = "Dépôt GitHub du Rapport",
+    progress.title     = "PROGRÈS DE LA COLLECTE DE DONNÉES SUR TERRAIN",
+    progress.records   = "La base de données contient %d enregistrements",
+    progress.last      = "dernier enregistrement du %s",
+    progress.partner   = "Collecte de données par",
+    progress.subtitle1 = "Progrès Géneral",
+    progress.women     = "Femmes Interviewées"
+  )
+)
